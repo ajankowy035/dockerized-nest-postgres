f4a076be59a71d77fd3616d9c81a8cd695dd71a4
ajankowy25 <agnieszka.jankowy@strategicimpact.io>
2022-06-21 12:29:07 +0200

    wallet unit tests

--- src/wallet/wallet.service.spec.ts
+++ src/wallet/wallet.service.spec.ts
@@ -1,12 +1,47 @@
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ShelterService } from './../shelter/shelter.service';
 import { WalletService } from './wallet.service';
+import { WalletEntity } from './models/wallet.entity';
+
+const mockRepository = jest.fn(() => ({
+  metadata: {
+    columns: [],
+    relations: [],
+  },
+}));
 
 describe('WalletService', () => {
   let service: WalletService;
+  let fakeShelterService: Partial<ShelterService>;
 
   beforeEach(async () => {
+    fakeShelterService = {
+      getAll: () =>
+        Promise.resolve([
+          { id: 1, name: 'Test Shelter', budget: 2100, donators: [] },
+        ]),
+      createShelter: () =>
+        Promise.resolve({
+          id: 1,
+          name: 'Test Shelter',
+          budget: 2100,
+          donators: [],
+        }),
+      findOne: (id) =>
+        Promise.resolve({
+          id,
+          name: 'Test Shelter',
+          budget: 2100,
+          donators: [],
+        }),
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WalletService],
+      providers: [
+        WalletService,
+        { provide: ShelterService, useValue: fakeShelterService },
+        { provide: getRepositoryToken(WalletEntity), useClass: mockRepository },
+      ],
     }).compile();
 
     service = module.get<WalletService>(WalletService);
