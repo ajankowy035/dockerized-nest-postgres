9b2d7983b7a1597d9895bcd2ba01af7579a953de
ajankowy25 <agnieszka.jankowy@strategicimpact.io>
2022-06-23 11:41:43 +0200

    serialize shelter and protect users

--- src/shelter/shelter.controller.ts
+++ src/shelter/shelter.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Body, Post, Get, Param } from '@nestjs/common';
 import { ShelterService } from './shelter.service';
 import { CreateShelterDto } from './dtos/create-shelter.dto';
+import { Serialize } from '../interceptors/serialize.intraceptor';
+import { ShelterDto } from './dtos/shelter.dto';
 
 @Controller('shelter')
 export class ShelterController {
   constructor(private shelterService: ShelterService) {}
 
   @Get()
+  @Serialize(ShelterDto)
   getAll() {
     return this.shelterService.getAll();
   }
 
   @Get('/:id')
+  @Serialize(ShelterDto)
   getOne(@Param('id') id: string) {
     return this.shelterService.findOne(parseInt(id));
   }
