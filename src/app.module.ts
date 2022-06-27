cb549ca7dfd5160cca910f02887093f235ebbcc0
ajankowy25 <agnieszka.jankowy@strategicimpact.io>
2022-06-27 09:56:36 +0200

    change typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,11 +42,11 @@ const cookieSession = require('cookie-session');
         return {
           type: 'postgres',
           url: configService.get('HEROKU_BD_URL'),
+          host: configService.get('DB_HOST'),
+          port: Number(configService.get('DB_PORT')),
           username: configService.get('DB_USER'),
           password: configService.get('DB_PASSWORD'),
           database: configService.get('DB'),
-          host: configService.get('DB_HOST'),
-          port: Number(configService.get('DB_PORT')),
           autoLoadEntities: true,
           synchronize: true,
           entities: [UserEntity, ShelterEntity, WalletEntity],
